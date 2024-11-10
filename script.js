359e4c8f974575c00a41ba1dab4ec4ac1d020415
Juan Diego Soto <hornodelaabue@gmail.com>
2024-11-10 16:10:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ const botonEnfoque = document.querySelector('.app__card-button--enfoque')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 const botones = document.querySelectorAll('.app__card-button')
+const inputEnfoqueMusica = document.querySelector('#alternar-musica')
+const musica = new Audio('./sonidos/luna-raise-part-one.mp3')
+const botonInciarPausar = document.querySelector('#start-pause')
+
+let tiempoTranscurridoEnSegundos = 5
+
+musica.loop = true 
+
+inputEnfoqueMusica.addEventListener('change', ()=> {
+    if (musica.paused){
+        musica.play()
+    }else musica.pause()
+})
 
 //El addEvenListener espera que suceda un evento, cuando este pase ejecuta una función.
 //La arrow function es una manera más corta de hacer una función
@@ -25,6 +38,11 @@ botonLargo.addEventListener('click', () => {
 });
 
 function cambiarContexto(contexto) {
+
+    botones.forEach(function(contexto){
+        contexto.classList.remove('active')
+    })
+
     html.setAttribute ('data-contexto', contexto)
     banner.setAttribute ('src', `/imagenes/${contexto}.png`)
 
@@ -47,3 +65,10 @@ function cambiarContexto(contexto) {
             break;
     }
 }
+
+const cuentaRegresiva = ()=> {
+    tiempoTranscurridoEnSegundos -= 1
+    console.log('Temporizador:' + tiempoTranscurridoEnSegundos)
+}
+
+botonInciarPausar.addEventListener('click')
